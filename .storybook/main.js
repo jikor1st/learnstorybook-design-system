672e4d03ee4d0d5e6e0ea0183b2d511267096cdb
jikor1st <jikor1st@naver.com>
2022-10-20 09:41:35 +0900

    update

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,18 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  // Changes the load order of our stories. First loads the Intro page
+  // automatically import all files ending in *.stories.js|mdx
+  stories: [
+    "../src/Intro.stories.mdx",
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
     "@storybook/preset-create-react-app",
+    "@storybook/addon-a11y",
+    "@storybook/addon-interactions",
   ],
   framework: "@storybook/react",
+  staticDirs: ["../public"],
 };
